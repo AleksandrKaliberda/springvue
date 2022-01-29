50810c2976ea1c80a6681111656b5461e1eedd57
AleksandrKaliberda <alexandr.kaliberda@gmail.com>
2022-01-29 20:01:46 +0200

    Initial commit

---
+++ backend/src/main/java/main/BackendApplication.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+    /**
+     * Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+        // Run the app!
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
