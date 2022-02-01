5c221e888b191710b432e67de6e45d6672a6b895
AleksandrKaliberda <alexandr.kaliberda@gmail.com>
2022-02-01 19:28:31 +0200

    second changes commit

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,4 +2,36 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+  <button
+  @click="getStoryWords"
+  >
+    Get story words
+  </button>
+
+  <div>{{storyWord}}</div>
 </template>
+
+<script>
+ import DataService from "../services/DataService"
+
+  export default {
+    name: "About",
+
+    data() {
+      return {
+        storyWord: "N/A"
+      }
+    },
+
+    methods:{
+      getStoryWords(){
+        DataService.getStoryWords()
+                .then(response =>{
+                  this.storyWord = response.data;
+                  console.log(this.storyWord);
+                })
+      }
+    }
+  }
+</script>
